b4a9b25000d69f965eb6cd070f9b5fc5c084cfc8
Thomas Gillis <thomasgillis@Thomas-Gilliss-MacBook-Pro.local>
2011-11-30 01:23:36 -0500

    templates, almost ready

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<pre>
 <?php
 
 class Tweets {
@@ -14,18 +13,23 @@ class Tweets {
     
     function __construct(){
         $this->accounts = array("libertysqga","libertysqga2","libertysqga3","libertysqga4");
-        
+        $this->route();
     }
+    
+    function route(){
+        $func = $_GET["p"];
+        $this->$func();
+    }
+    
     
     function get(){
         //https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=libertysqga&count=20
         $all_tweets = array();
         foreach ($this->accounts as $account){
-             $tweets = file_get_contents("https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=".$account."&count=100");         
+             $tweets = file_get_contents("https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=".$account."&count=1000");         
             $tweets = json_decode($tweets,true);
-            echo count($tweets);
             foreach ($tweets as $t) {
-                $all_tweets[$t["id_str"]] = array("time"=>date("Y-m-d H-is" ,strtotime($t["created_at"])),"date"=>date("Y-m-d",strtotime($t["created_at"])),"text"=>$t["text"]);
+                $all_tweets[$t["id_str"]] = array("time"=>date("Y-m-d H:i:s" ,strtotime($t["created_at"])),"date"=>date("Y-m-d",strtotime($t["created_at"])),"text"=>$t["text"]);
             }
         } 
         ksort($all_tweets);
@@ -34,7 +38,6 @@ class Tweets {
     }
     
     function write($tweets){
-        $filename = date("Y-m-d",time()).".json";
         //tweets come in as an array;
         //split the array by day;
         $tweets_by_day = array();
@@ -44,7 +47,7 @@ class Tweets {
         
         foreach ($tweets_by_day as $date=>$tweets) {
             //file name is just today's date, whatever that may be
-            $filename = $date.".json";
+            $filename = "archive/".$date.".json";
             //tweets to text
             $tweets = json_encode($tweets);
             //rewrite the file
@@ -52,62 +55,25 @@ class Tweets {
         }
     }
     
-    function dispay(){
-        
+    function display($date=null){
+        if (isset($_GET["date"])) {
+            $date = $_GET["date"];
+        }
+        if ($date==null) {
+            $date = date("Y-m-d",time());
+        }
+        $tweets = file_get_contents("archive/".$date.".json");
+        $tweets = json_decode($tweets,true);
+        $archives = scandir("archive/");
+        unset($archives[0]); unset($archives[1]);
+        $new = array();
+        foreach ($archives as $a) {
+            $new[] = array("filename"=>$a,"date"=>str_replace(".json","",$a));
+        }
+        $archives = array_reverse($new);
+        include ("templates/display.php");
     }
 }
 
 $t = new Tweets();
-$t->get();
 exit();
-function eviction_tweets($page) {
-      $tweets = ORM::factory('message')->find_all();
-      $i=$page;
-      
-      $hashtags = array("%23occupyla","%23lapd","%23oo");
-      foreach ($hashtags as $hashtag) { 
-        $finished=1;
-        while ($i < 100 && ($finished==1)){
-          //get max_id
-          //look for the hashtags in question = zuccotti, ows, ofs
-
-          $max_id = $t->find(array(),array("id_str"=>true))->sort(array("id_str"=>1))->limit(1);
-          $max_id = $max_id->getNext();
-          $max_id = $max_id["id_str"];
-          $max_id_string = ($max_id==null) ? "" :"+max_id%3A".$max_id;
-              while ($page < 16 &&($finished==1)) {
-                    $tweets = file_get_contents( "https://search.twitter.com/search.json?q=".$hashtag."+until%3A2011-11-29+since%3A2011-11-27".$max_id_string."&result_type=recent&include_entities=true&rpp=100&page=".$page);
-                //query string for the whole query, so no page or max id
-                $query_string = "https://search.twitter.com/search.json?q=%23".$hashtag."+until%3A2011-11-29+since%3A2011-11-27&result_type=recent&include_entities=true";
-                $tweets = json_decode($tweets,true);
-                foreach ($tweets["results"] as $tweet) {
-
-                  //add some metadata from the crawler
-                  $tweet["ows_meta"] = array("query_string"=>$query_string,"crawl_timestamp"=>time());
-
-                  $t->update(array("id_str"=>$tweet["id_str"]), $tweet ,array("upsert"=>true));
-                }
-                $page = $page +1;
-                if (count($tweets["results"])>0) {
-                  sleep(15);
-                  $finished=1;
-                } else {
-                  $finished = 0;
-                }
-              }
-          $page = 1;
-          $i=$i+1;
-        }
-      
-      //upsert these to the main tweets table
-      $alltweets = $t->find();
-      $maintweets = $ows->selectCollection("tweets");
-      while ($tweet = $alltweets->getNext()) {
-        $maintweets->update(array("id_str"=>$tweet["id_str"]), $tweet ,array("upsert"=>true));
-      }
-      //clear the staging table
-      $t->remove(array());
-      }
-      exit();
-      
-}
---
+++ templates/display.php
@@ -0,0 +1,62 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css" type="text/css" media="screen" title="no title" charset="utf-8" />
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <div id="header">
+            <h1>Tweets for <?=date("l, F j, Y",strtotime($date))?></h1>
+       <!-- date picker-->
+            <div id="menu">
+                <form action="#">
+                    <select id="date-chooser">
+                        <?php if (isset($archives) && is_array($archives) && (count($archives) >0)):?>
+                            <option value="">--select date--</option>
+                        <?php foreach ($archives as $archive):?>
+                            <option value="<?=$archive["date"]?>"><?=$archive["date"]?></option>
+                        <?php endforeach;?>
+
+                        <?php else: ?>
+                        
+                        <?php endif;?>
+                        
+                    </select>
+                </form>
+                
+            </div>
+            
+        </div>
+        <!--next / prev-->
+        <div id="tweets">
+            <?php if (isset($tweets) && is_array($tweets) && (count($tweets) >0)):?>
+            <?php foreach ($tweets as $tweet):?>
+            <div class="tweet">
+                <?php //only every 15 mins?>
+                <div class="date">
+                    <?=date("H:i a",strtotime($tweet["time"]))?><br/>
+                </div>
+                <div class="text">
+                    <?=$tweet["text"]?><br/>
+                </div>
+            </div>
+            
+            <?php //only every 15 mins?>
+            <div class="divider">
+            </div>
+            
+            <?php endforeach;?>
+            <?php else: ?>
+            <?php endif;?>
+        </div>
+        
+       <script type="text/javascript">
+       $(document).ready(function(){
+           $("#date-chooser").change(function(){
+               var url = "?p=display&date="+$("#date-chooser").val();
+               window.location.href = url;
+           })
+       })
+       </script> 
+        <!--next / prev-->
+    </body>
+</html>
